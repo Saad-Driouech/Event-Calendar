4e8d5b4efc0c3f990651020c1aba59c473472f1f
Saad-Driouech <73725166+Saad-Driouech@users.noreply.github.com>
2021-07-07 12:07:13 +0100

    calendarapp/forms.py

--- calendarapp/forms.py
+++ calendarapp/forms.py
@@ -1,6 +1,6 @@
 from django.db.models import fields
-from django.forms import ModelForm, DateInput
-from calendarapp.models import Course, Professor, Session
+from django.forms import ModelForm, DateInput, widgets, TimeInput
+from calendarapp.models import Course, Professor, Session, DayAvailability
 from django import forms
 
 class SessionForm(ModelForm):
@@ -30,20 +30,25 @@ class AddProfessorForm(forms.ModelForm):
   class Meta:
     model = Professor
 
-    widgets = {
-      'start_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
-      'end_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
-    }
-    
-    fields = ['name', 'start_time', 'end_time', 'course']
-
-  def __init__(self, *args, **kwargs):
-    super(AddProfessorForm, self).__init__(*args, **kwargs)
-    # input_formats to parse HTML5 datetime-local input to datetime field
-    self.fields['start_time'].input_formats = ('%Y-%m-%dT%H:%M',)
-    self.fields['end_time'].input_formats = ('%Y-%m-%dT%H:%M',)
+    fields = ['name', 'course', 'rank']
 
 class AddCourseFrom(forms.ModelForm):
   class Meta:
     model = Course
     fields = ['title', 'code']
+
+class DayAvailabilityForm(forms.ModelForm):
+  class Meta:
+    model = DayAvailability
+
+    widgets = {
+      'start_time': TimeInput(attrs={'type': 'time'}, format='%H:%M'),
+      'end_time': TimeInput(attrs={'type': 'time'}, format='%H:%M'),
+    }
+    fields= ['professor', 'day', 'start_time', 'end_time']
+
+  def __init__(self, *args, **kwargs):
+    super(DayAvailabilityForm, self).__init__(*args, **kwargs)
+    # input_formats to parse HTML5 datetime-local input to datetime field
+    self.fields['start_time'].input_formats = ('%H:%M',)
+    self.fields['end_time'].input_formats = ('%H:%M',)
