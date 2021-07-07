7f1f056e9cf88c8fae79d17155f19ede94969feb
Saad-Driouech <73725166+Saad-Driouech@users.noreply.github.com>
2021-07-07 12:15:28 +0100

    updating utils because of new namings (nothing to worry about)

--- calendarapp/utils.py
+++ calendarapp/utils.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime, timedelta
 from calendar import HTMLCalendar
-from .models import Event
+from .models import Session
 from eventcalendar.helper import get_current_user
 
 class Calendar(HTMLCalendar):
@@ -34,7 +34,7 @@ class Calendar(HTMLCalendar):
 	# formats a month as a table
 	# filter events by year and month
 	def formatmonth(self, withyear=True):
-		events = Event.objects.filter(start_time__year=self.year, start_time__month=self.month)
+		events = Session.objects.filter(start_time__year=self.year, start_time__month=self.month)
 
 		cal = f'<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
 		cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
