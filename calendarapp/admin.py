11f6d8c1b17d389598c49311d3743284fc3b8c6b
Saad-Driouech <73725166+Saad-Driouech@users.noreply.github.com>
2021-07-07 12:06:42 +0100

    calendarapp/admin.py

--- calendarapp/admin.py
+++ calendarapp/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from calendarapp.models import Course, Professor, Session
+from calendarapp.models import Course, DayAvailability, Professor, Session, Venue
 
-class EventMemberAdmin(admin.ModelAdmin):
+"""class EventMemberAdmin(admin.ModelAdmin):
     model = Professor
-    list_display = ['event', 'name']
+    list_display = ['event', 'name']"""
 
 admin.site.register(Session)
-admin.site.register(Professor, EventMemberAdmin)
+admin.site.register(Professor)
 admin.site.register(Course)
+admin.site.register(Venue)
+admin.site.register(DayAvailability)
