2b3a6589d85d182ab698caef44e7ab257d637907
Saad-Driouech <73725166+Saad-Driouech@users.noreply.github.com>
2021-07-07 12:12:53 +0100

    updating urls to accomodate for new views

--- calendarapp/urls.py
+++ calendarapp/urls.py
@@ -6,9 +6,11 @@ app_name = 'calendarapp'
 urlpatterns = [
     path('index', views.index, name='index'),
     path('', views.CalendarView.as_view(), name='calendar'),
-    path('event/new/', views.create_session, name='event_new'),
+    path('session/new/', views.create_session, name='session_new'),
+    path('course/new/', views.create_course, name='course_new'),
     path('event/edit/<int:pk>/', views.SessionEdit.as_view(), name='event_edit'),
     path('event/<int:event_id>/details/', views.session_details, name='event-detail'),
-    path('add_eventmember/<int:session_id>', views.assign_professor, name='add_eventmember'),
+    path('professor/create/', views.create_professor, name='professor_new'),
     path('event/<int:pk>/remove', views.EventMemberDeleteView.as_view(), name="remove_event"),
+    path('availability/create/',views.create_availablity, name='availability_new')
 ]
